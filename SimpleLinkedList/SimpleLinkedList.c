aec8d358b513f0cfdfa89067b3b3767c8a6816da
Arpan <arpan.patel@vayavyalabs.com>
2013-07-16 18:29:00 +0530

    Added Apis for Add Node in between and Delete any node.
    Signed-off-by: Arpan <arpan.patel@vayavyalabs.com>

--- SimpleLinkedList/SimpleLinkedList.c
+++ SimpleLinkedList/SimpleLinkedList.c
@@ -23,6 +23,7 @@ void DisplayList(struct node *headref) {
     }
 }
 
+/* Adds Node at Tail */
 void AppendToList(struct node **headref, int val) {
     struct node *curr = (struct node *)malloc(sizeof(struct node));
     struct node *lastNode = *headref;
@@ -40,6 +41,63 @@ void AppendToList(struct node **headref, int val) {
     }
 }
 
+/*
+ * TODO: Check Whether headref is NULL or not
+ */
+void AppendAtPosition(struct node **headref, int findValue, int newValue) {
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    struct node *curr = NULL;
+    int i = 0;
+
+    for (curr = (*headref); curr->val != findValue; curr = curr->next) {
+        printf(" LL %d - %d\n", i++, curr->val);
+    }
+
+    newNode->val = newValue;
+    newNode->next = curr->next;
+    curr->next = newNode;
+}
+
+void DeleteNode(struct node **headref, int Value) {
+    struct node *curr = (*headref);
+    struct node *prev = NULL;
+    int i = 0;
+
+    if (curr == NULL)
+        printf("List Empty\n");
+
+
+    prev = NULL;
+    curr = (*headref);
+    
+    if (curr->next == NULL) {
+        printf("Only one node, i.e. Head\n");
+        if (curr->val == Value) {
+            free(curr);
+            printf("List Emptied\n");
+        } else {
+            printf("Node Not Found\n");
+            return;
+        }
+    }
+
+    curr = curr->next;
+    if (curr->val == Value) {
+       prev->next = curr->next;
+       free(curr);
+    }
+
+    for (curr = (*headref), prev = curr; curr->val != Value; prev = curr, curr = curr->next) {
+        printf("#LL %d - %d\n", i++, curr->val);
+    }
+    
+    prev->next = curr->next;
+    free(curr);
+    printf("Node Deleted\n");
+
+
+}
+
 int main() {
     printf("Single Linked List\n");
 
@@ -51,8 +109,13 @@ int main() {
     InsertNode(&head, 3);
     AppendToList(&head, 8);
     AppendToList(&head, 9);
+    AppendAtPosition(&head, 8, 7);
 
     DisplayList(head);
+
+    DeleteNode(&head, 1);
+    DisplayList(head);
+
 
     return 0;
 }
